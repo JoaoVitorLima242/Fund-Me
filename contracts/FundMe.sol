21fe5d8baf04c1e6befeaec90e6d0d49786205f3
JoaoVitorLima242 <jlima0500@gmail.com>
2024-10-24 13:00:33 -0300

    create fund function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,19 +5,34 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe {
     address ethUsdPriceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-    uint256 minimumInUsd = 5;
+    uint256 minimumInUsd = 5e18;
     AggregatorV3Interface priceFeed;
 
     constructor() {
         priceFeed = AggregatorV3Interface(ethUsdPriceFeedAddress);
     }
 
-    function fund() public {}
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumInUsd, "didn't send enough ETH");
+    }
 
     function withdraw() public {}
 
-    function getPrice() public view returns(uint256) {
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer *1e10);
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        int256 answerWithTenZeros = answer * 1e10;
+
+        return uint256(answerWithTenZeros);
     }
 }
