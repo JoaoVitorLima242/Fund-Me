fd3d9f4c424206f42c90dd3d244a867fd9750889
JoaoVitorLima242 <jlima0500@gmail.com>
2024-10-24 12:42:25 -0300

    get eth price using chainlink

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    function fund () public {}
-    function withdraw () public {}
+    address ethUsdPriceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    uint256 minimumInUsd = 5;
+    AggregatorV3Interface priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(ethUsdPriceFeedAddress);
+    }
+
+    function fund() public {}
+
+    function withdraw() public {}
+
+    function getPrice() public view returns(uint256) {
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer *1e10);
+    }
 }
